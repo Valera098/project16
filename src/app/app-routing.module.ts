0f59d5bbd05004edfbf460cffe9bf6ae861d10e9
Valera098 <66243570+Valera098@users.noreply.github.com>
2021-03-29 07:57:41 +0300

    сделал

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { GroupComponent } from './group/group.component';
+import { InformationComponent } from './information/information.component';
+import { AddworkersComponent } from './addworkers/addworkers.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InformationComponent,
+  },
+  {
+    path: 'group/:group',
+    component: GroupComponent,
+  },
+  {
+    path: 'addworker',
+    component: AddworkersComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
